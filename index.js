f9e2187a0d9d133cecab0b15b73aca814c907770
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-09 16:07:10 +0300

    changes

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const multiply = 31 * 2
- 
-const random = 1 > 0
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-const mod = 24 / 4
+const random = Math.floor(Math.random() *10) + 1;
 
-const max = 20
+const num3 = 34;
+const num4 = 7;
+const mod = num3 % num4;
+
+const max = 20;
