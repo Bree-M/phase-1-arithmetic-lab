c126aaaf867cd92bf4cdaba1dfdeb88831ba2635
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-09 16:21:05 +0300

    commit my change

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const multiply = num1 * num2;
 
 const random = Math.floor(Math.random() *10) + 1;
 
-const num3 = 23;
-const num4 = 5;
+const num3 = 28;
+const num4 = 6;
 const mod = num3 % num4;
 
 const max = 20;
